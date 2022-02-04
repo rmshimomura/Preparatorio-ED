42d41129b86d4098ada51bdb86bbf5b23bd5dcd0
rmshimomura <shimomurarodrigo1@gmail.com>
2022-02-04 19:48:18 -0300

    Makefile feito

---
+++ Tutoriais/Makefile/arquivo3.c
@@ -0,0 +1,5 @@
+#include "header3.h"
+
+void function_3() {
+    puts("Ola da funcao 3!!!");
+}
---
+++ Tutoriais/Makefile/main.c
@@ -0,0 +1,12 @@
+#include "libs.h"
+#include "header1.h"
+#include "header2.h"
+#include "header3.h"
+
+int main() {
+
+    function_1();
+    function_2();
+    function_3();
+
+}
